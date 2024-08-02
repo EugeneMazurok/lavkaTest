3b528ba91e2323730f541174bce747bd0374d782
EugeneMazurok <audiofaun896373@gmail.com>
2024-08-02 19:35:59 +0300

    исправил исправления

--- src/views/Search/Index.vue
+++ src/views/Search/Index.vue
@@ -1,12 +1,10 @@
 <script setup>
-
-import { ref, onMounted, onUnmounted, nextTick, onActivated, onDeactivated } from 'vue'
+import { ref, onMounted, nextTick, onActivated, onDeactivated } from 'vue'
 import Item from '../../components/Index/Games/Item.vue'
 import El from '../../components/Index/Donations/El.vue'
 import { createDirectus, rest, readItems } from '@directus/sdk'
 import config from '../../config/config.json'
 import { useRouter } from 'vue-router'
-import Header from '../../components/Header.vue'
 import { Icon } from '@iconify/vue'
 import scrollDown from "../../utils/scrollDown.js";
 
@@ -33,82 +31,83 @@ const searchValue = ref('')
 const sale_prices = ref(null)
 
 const getStartParams = async () => {
-    let response = await client.request(readItems('start_params', {
-        fields: ['sale_prices']
-    }))
+  let response = await client.request(readItems('start_params', {
+    fields: ['sale_prices']
+  }))
 
-    sale_prices.value = response.sale_prices
+  sale_prices.value = response.sale_prices
 }
 
 const search = async () => {
-    if (searchValue.value.length >= 3) {
-        loading.value = true
+  if (searchValue.value.length >= 3) {
+    loading.value = true
 
-        window.localStorage.setItem('search', searchValue.value)
+    window.sessionStorage.setItem('search', searchValue.value)
 
-        games.value = await getGames()
-        donations.value = await getDonations()
+    games.value = await getGames()
+    donations.value = await getDonations()
 
-        loading.value = false
-    }
+    loading.value = false
+  }
 }
 
 const getGames = async () => {
+  loading.value = true
 
-    loading.value = true
+  let response = await client.request(readItems('Games', {
+    fields: ['*.*.*'],
+    filter: {
+      status: {
+        "_eq": "published"
+      },
+      platform: {
+        "_eq": platform.value
+      },
+      _or: [
+        { title: { "_icontains": searchValue.value } },
+        { meta: { "_icontains": searchValue.value } }
+      ]
+    },
+    limit: 25
+  }))
 
-    let response = await client.request(readItems('Games', {
-        fields: ['*.*.*'],
-        filter: {
-            status: {
-                "_eq": "published"
-            },
-            platform: {
-                "_eq": platform.value
-            },
-            _or: [
-                { title: { "_icontains": searchValue.value } },
-                { meta: { "_icontains": searchValue.value } }
-            ]
-        },
-        limit: 25
-    }))
+  loading.value = false
 
-
-    return response
-
+  return response
 }
 
 const getDonations = async () => {
-    loading.value = true
+  loading.value = true
 
-    let response = await client.request(readItems('Donations', {
-        fields: ['*.*.*'],
-        filter: {
-            status: {
-                "_eq": "published"
-            },
-            _or: [
-                {
-                    platform: {
-                        "_eq": platform.value
-                    },
-                },
-                {
-                    other: {
-                        "_eq": 'Yes'
-                    },
-                },
-            ],
-            _or: [
-                { title: { "_icontains": searchValue.value } },
-                { meta: { "_icontains": searchValue.value } }
-            ]
+  let response = await client.request(readItems('Donations', {
+    fields: ['*.*.*'],
+    filter: {
+      status: {
+        "_eq": "published"
+      },
+      _or: [
+        {
+          platform: {
+            "_eq": platform.value
+          },
         },
-        limit: 25
-    }))
+        {
+          other: {
+            "_eq": 'Yes'
+          },
+        },
+      ],
+      _or: [
+        { title: { "_icontains": searchValue.value } },
+        { meta: { "_icontains": searchValue.value } }
+      ]
+    },
+    limit: 25
+  }))
 
-    return response
+  loading.value = false
+
+  return response
 }
 
 const scrollableElement = ref(null)
@@ -121,97 +120,91 @@ const logCurrentScrollPosition = () => {
 }
 
 onActivated(async () => {
-    webapp.onEvent('backButtonClicked', back)
-    webapp.BackButton.show()
+  webapp.onEvent('backButtonClicked', back)
+  webapp.BackButton.show()
 
-    await nextTick()
-    inputRef.value.focus()
-    scrollDown(scrollableElement, currentScrollPosition)
+  await nextTick()
+  inputRef.value.focus()
+  scrollDown(scrollableElement, currentScrollPosition)
 })
 
 onDeactivated(() => {
-    webapp.offEvent('backButtonClicked', back)
-    window.localStorage.removeItem('search')
-    webapp.BackButton.hide()
+  webapp.offEvent('backButtonClicked', back)
+  webapp.BackButton.hide()
 })
 
 onMounted(async () => {
-    loading.value = true
+  loading.value = true
 
-    const activeTab = window.localStorage.getItem('activeTab')
-    if (activeTab) {
-        platform.value = JSON.parse(activeTab).platform
-    }
+  const activeTab = window.localStorage.getItem('activeTab')
+  if (activeTab) {
+    platform.value = JSON.parse(activeTab).platform
+  }
 
-    let savedSearch = window.localStorage.getItem('search')
-    if (savedSearch) {
-        searchValue.value = savedSearch
-    }
+  let savedSearch = window.sessionStorage.getItem('search')
+  if (savedSearch) {
+    searchValue.value = savedSearch
+  }
 
-    await getStartParams()
+  await getStartParams()
+  await search()
 
-    await search()
-
-    loading.value = false
-
+  loading.value = false
 })
-
 </script>
 
 <template>
-    <div class="overflow-y-auto h-screen pt-20 flex flex-col" ref="scrollableElement" @scroll="logCurrentScrollPosition">
-        <div class="relative flex flex-col">
+  <div class="overflow-y-auto h-screen pt-20 flex flex-col" ref="scrollableElement" @scroll="logCurrentScrollPosition">
+    <div class="relative flex flex-col">
+      <div v-if="!webapp.initDataUnsafe.user" class="px-4">
+        <button @click="back" class="flex bg-blue w-fit pl-2 pr-4 py-1.5 rounded-xl items-center gap-x-1 font-medium">
+          <Icon icon="ion:chevron-back-outline" />
+          <span>Назад</span>
+        </button>
+      </div>
 
-            <div v-if="!webapp.initDataUnsafe.user" class="px-4">
-                    <button @click="back" class="flex bg-blue w-fit pl-2 pr-4 py-1.5 rounded-xl items-center gap-x-1 font-medium">
-                        <Icon icon="ion:chevron-back-outline" />
-                        <span>Назад</span>
-                    </button>
-                </div>
-
-            <header class="flex justify-center p-4 gap-x-2 fixed z-20 left-0 right-0 top-0 bg-bg_color">
-               <div class="w-full min-512:w-[480px]">
-                    <div class="flex transition-all items-center gap-x-2.5 text-left bg-hint_bg_color rounded-xl h-12 px-4 w-full">
-                        <Icon icon="ion:search" class="text-xl shrink-0" />
-                        <input v-model="searchValue" @keyup.enter="(e) => { e.target.blur(); search() }" ref="inputRef" class="placeholder:text-hint_color line-clamp-1 text-sm w-full h-full outline-none bg-transparent" placeholder="Найти игру" />
-                    </div>
-               </div>
-            </header>
-
-            <transition name="fade" appear>
-                <div v-if="games && donations" :class="['pb-4 flex flex-col gap-y-2']">
-                    <div v-if="games.length < 1 && donations.length < 1" class="px-4 py-4 flex justify-center">
-                        <span class="text-sm text-hint_color text-semibold">Хм, ничего не найдено :(</span>
-                    </div>
-
-                    <div v-if="games.length > 0 || donations.length > 0" class="grid grid-cols-2 gap-4 p-4" v-auto-animate>
-                        <Item 
-                            v-for="game, index in games"
-                            :key="index"
-                            :product="game"
-                            :sale_prices="sale_prices"
-                        />
-
-                        <El 
-                            v-for="donation, index in donations"
-                            :key="index"
-                            :product="donation"
-                        />
-                    </div>
-
-                    <div class="flex justify-center flex-col items-center gap-y-2.5 px-4">
-                        <span class="font-medium">Не нашли нужную игру? Напишите нам!</span>
-                        <a :class="['w-full rounded-xl px-4 py-2.5 text-center shadow-sm active:opacity-75 font-medium', platform === 'Xbox' ? 'bg-green' : 'bg-blue']" href="https://t.me/lavka_assistant" target="_blank">Написать Продавцу Лавки</a>
-                    </div>
-                </div>
-                
-            </transition>
-
-            <transition name="fade">
-                <div v-if="loading" class="grid grid-cols-2 gap-4 p-4">
-                    <div v-for="el in 3" class="flex flex-col h-52 bg-hint_bg_color animate-pulse rounded-xl overflow-clip w-full" />
-                </div>
-            </transition>
+      <header class="flex justify-center p-4 gap-x-2 fixed z-20 left-0 right-0 top-0 bg-bg_color">
+        <div class="w-full min-512:w-[480px]">
+          <div class="flex transition-all items-center gap-x-2.5 text-left bg-hint_bg_color rounded-xl h-12 px-4 w-full">
+            <Icon icon="ion:search" class="text-xl shrink-0" />
+            <input v-model="searchValue" @keyup.enter="(e) => { e.target.blur(); search() }" ref="inputRef" class="placeholder:text-hint_color line-clamp-1 text-sm w-full h-full outline-none bg-transparent" placeholder="Найти игру" />
+          </div>
         </div>
+      </header>
+
+      <transition name="fade" appear>
+        <div v-if="games && donations" :class="['pb-4 flex flex-col gap-y-2']">
+          <div v-if="games.length < 1 && donations.length < 1" class="px-4 py-4 flex justify-center">
+            <span class="text-sm text-hint_color text-semibold">Хм, ничего не найдено :(</span>
+          </div>
+
+          <div v-if="games.length > 0 || donations.length > 0" class="grid grid-cols-2 gap-4 p-4" v-auto-animate>
+            <Item
+                v-for="game, index in games"
+                :key="index"
+                :product="game"
+                :sale_prices="sale_prices"
+            />
+
+            <El
+                v-for="donation, index in donations"
+                :key="index"
+                :product="donation"
+            />
+          </div>
+
+          <div class="flex justify-center flex-col items-center gap-y-2.5 px-4">
+            <span class="font-medium">Не нашли нужную игру? Напишите нам!</span>
+            <a :class="['w-full rounded-xl px-4 py-2.5 text-center shadow-sm active:opacity-75 font-medium', platform === 'Xbox' ? 'bg-green' : 'bg-blue']" href="https://t.me/lavka_assistant" target="_blank">Написать Продавцу Лавки</a>
+          </div>
+        </div>
+      </transition>
+
+      <transition name="fade">
+        <div v-if="loading" class="grid grid-cols-2 gap-4 p-4">
+          <div v-for="el in 3" class="flex flex-col h-52 bg-hint_bg_color animate-pulse rounded-xl overflow-clip w-full" />
+        </div>
+      </transition>
     </div>
+  </div>
 </template>
