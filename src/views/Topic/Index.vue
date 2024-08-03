e4384e0c5aa477c9976680a40b9b13e3a5603b64
EugeneMazurok <audiofaun896373@gmail.com>
2024-08-03 15:25:27 +0300

    исправил баг с фильтрами

--- src/views/Topic/Index.vue
+++ src/views/Topic/Index.vue
@@ -1,6 +1,6 @@
 <script setup>
 
-import { ref, reactive, onMounted, onUnmounted, onActivated, onDeactivated } from 'vue'
+import { ref, reactive, onMounted, onActivated, onDeactivated } from 'vue'
 import Item from '../../components/Index/Games/Item.vue'
 import { createDirectus, rest, readItems } from '@directus/sdk'
 import config from '../../config/config.json'
@@ -8,17 +8,18 @@ import { useRoute, useRouter } from 'vue-router'
 import Header from '../../components/Header.vue'
 import { Icon } from '@iconify/vue'
 import scrollDown from "../../utils/scrollDown.js";
+import { debounce } from "lodash";
 
 const webapp = window.Telegram.WebApp
 
 const router = useRouter()
 
 const back = () => {
-    if (window.history.length > 1) {
-        router.go(-1)
-    } else {
-        router.push({ name: 'INDEX' })
-    }
+  if (window.history.length > 1) {
+    router.go(-1)
+  } else {
+    router.push({ name: 'INDEX' })
+  }
 }
 
 const route = useRoute()
@@ -27,14 +28,16 @@ const client = createDirectus(config.DIRECTUS.API).with(rest())
 
 const loading = ref(false)
 
-const games = ref(null)
+const games = ref([])
+const tempGames = ref([])
+const animating = ref(false);
 
 const page = ref(1)
 const pageSize = ref(50)
 
 const sort = reactive({
-    price: false,
-    title: false
+  price: false,
+  title: false
 })
 
 const orderBy = route.params.id
@@ -44,112 +47,97 @@ const platform = ref('Xbox')
 const sale_prices = ref(null)
 
 const getStartParams = async () => {
-    let response = await client.request(readItems('start_params', {
-        fields: ['sale_prices']
-    }))
-
-    sale_prices.value = response.sale_prices
+  let response = await client.request(readItems('start_params', {
+    fields: ['sale_prices']
+  }))
+  sale_prices.value = response.sale_prices
 }
 
 const getGames = async () => {
+  loading.value = true;
 
-    loading.value = true
+  let filter;
+  let sortParam = [];
 
-    let filter
+  if (orderBy === 'sale') {
+    filter = { sale_date_end: { "_nnull": true } };
+  } else if (orderBy === 'new') {
+    filter = { new: { "_eq": 'Yes' } };
+  } else if (orderBy === 'preorder') {
+    filter = { preorder: { "_eq": 'Yes' } };
+  } else if (orderBy === 'popular') {
+    filter = { popular: { "_eq": 'Yes' } };
+  }
 
-    if (orderBy === 'sale') {
-        filter = {
-            sale_date_end: {
-                "_nnull": true
-            }
-        }
-    } else
-    if (orderBy === 'new') {
-        filter = {
-            new: {
-                "_eq": 'Yes'
-            }
-        }
-    } else
-    if (orderBy === 'preorder') {
-        filter = {
-            preorder: {
-                "_eq": 'Yes'
-            }
-        }
-    } else
-    if (orderBy === 'popular') {
-        filter = {
-            popular: {
-                "_eq": 'Yes'
-            }
-        }
-    }
+  if (sort.price) {
+    sortParam.push(sort.price === 'lte' ? 'price_standart' : '-price_standart');
+  } else if (sort.title) {
+    sortParam.push('title');
+  }
 
+  try {
     let response = await client.request(readItems('Games', {
-        fields: ['*.*.*'],
-        filter: {
-            status: {
-                "_eq": "published"
-            },
-            platform: {
-                "_eq": platform.value
-            },
-            ...filter
-        },
-        page: page.value,
-        limit: pageSize.value
-    }))
+      fields: ['*.*.*'],
+      filter: {
+        status: { "_eq": "published" },
+        platform: { "_eq": platform.value },
+        ...filter
+      },
+      page: page.value,
+      limit: pageSize.value,
+      sort: sortParam.length ? sortParam : []
+    }));
 
     if (response && response.length < pageSize.value) {
-        ended.value = true
+      ended.value = true;
     }
-
-    return response
-
+    console.log(response)
+    return response;
+  } catch (error) {
+    console.error('Error fetching games:', error);
+    loading.value = false;
+    throw error;
+  }
 }
 
 const scrollableElement = ref(null)
 const currentScrollPosition = ref(0)
 
-const logCurrentScrollPosition = () => {
+const logCurrentScrollPosition = debounce(() => {
   if (scrollableElement.value) {
     currentScrollPosition.value = scrollableElement.value.scrollTop
     checkScroll()
   }
-}
+}, 50)
 
 onActivated(() => {
-    webapp.onEvent('backButtonClicked', back)
-    webapp.BackButton.show()
-
-    scrollDown(scrollableElement, currentScrollPosition)
+  webapp.onEvent('backButtonClicked', back)
+  webapp.BackButton.show()
+  scrollDown(scrollableElement, currentScrollPosition)
 })
 
 onDeactivated(() => {
-    webapp.offEvent('backButtonClicked', back)
-    webapp.BackButton.hide()
+  webapp.offEvent('backButtonClicked', back)
+  webapp.BackButton.hide()
 })
 
 onMounted(async () => {
-    loading.value = true
-
-    const activeTab = window.localStorage.getItem('activeTab')
-    if (activeTab) {
-        platform.value = JSON.parse(activeTab).platform
-    }
-
-    games.value = await getGames()
-    await getStartParams()
-    loading.value = false
+  loading.value = true
+  const activeTab = window.localStorage.getItem('activeTab')
+  if (activeTab) {
+    platform.value = JSON.parse(activeTab).platform
+  }
+  games.value = await getGames()
+  await getStartParams()
+  loading.value = false
 })
 
 const checkScroll = () => {
-    if (scrollableElement.value.scrollTop + scrollableElement.value.clientHeight >= scrollableElement.value.scrollHeight - 30) {
-        if (!loading.value) {
-            refreshHandler()
-        }
+  if (scrollableElement.value.scrollTop + scrollableElement.value.clientHeight >= scrollableElement.value.scrollHeight - 30) {
+    if (!loading.value) {
+      refreshHandler()
     }
+  }
 }
 
 const ended = ref(false)
@@ -158,48 +146,69 @@ const refreshHandler = async () => {
   if (!ended.value) {
     loading.value = true;
     page.value = page.value + 1;
-
+    console.log(`Текущая страница: ${page.value}`);
     setTimeout(async () => {
       let next_items = await getGames();
       if (!next_items) return;
       games.value = [...games.value, ...next_items];
       loading.value = false;
-    }, 200);
+    }, 100);
   }
 };
 
 function sortProducts() {
-    games.value.sort((a, b) => {
-        let priceA
-        let priceB
+  // Сброс страницы на 1 и очищаем текущее состояние списка игр
+  page.value = 1
+  ended.value = false
 
-        if (sale_prices.value.includes(platform.value)) {
-            priceA = a.price_subscription || a.price_sale || a.price_standart;
-            priceB = b.price_subscription || b.price_sale || b.price_standart;
-        } else {
-            priceA = a.price_standart;
-            priceB = b.price_standart;
-        }
+  // Устанавливаем флаг анимации
+  animating.value = true
 
-        if (sort.price === 'gte') {
-            if (priceA < priceB) return 1;
-            if (priceA > priceB) return -1;
-        } else if (sort.price === 'lte') {
-            if (priceA < priceB) return -1;
-            if (priceA > priceB) return 1;
-        }
+  // Загрузка игр с новым порядком сортировки
+  getGames().then(newGames => {
+    tempGames.value = newGames // Временно храним новые игры
+    sortGames() // Сортируем игры
+    games.value = [...tempGames.value] // Обновляем отображение списка игр
+    loading.value = false // Отключаем индикатор загрузки
 
-        if (sort.title) {
-            const titleA = a.title.toLowerCase();
-            const titleB = b.title.toLowerCase();
-            if (titleA < titleB) return -1;
-            if (titleA > titleB) return 1;
-        }
-
-        return 0;
-    });
+    // Убираем флаг анимации только после завершения обновления игр
+    animating.value = false
+  })
 }
 
+function sortGames() {
+  if (tempGames.value) {
+    tempGames.value.sort((a, b) => {
+      let priceA
+      let priceB
+
+      if (sale_prices.value.includes(platform.value)) {
+        priceA = a.price_subscription || a.price_sale || a.price_standart;
+        priceB = b.price_subscription || b.price_sale || b.price_standart;
+      } else {
+        priceA = a.price_standart;
+        priceB = b.price_standart;
+      }
+
+      if (sort.price === 'gte') {
+        if (priceA < priceB) return 1;
+        if (priceA > priceB) return -1;
+      } else if (sort.price === 'lte') {
+        if (priceA < priceB) return -1;
+        if (priceA > priceB) return 1;
+      }
+
+      if (sort.title) {
+        const titleA = a.title.toLowerCase();
+        const titleB = b.title.toLowerCase();
+        if (titleA < titleB) return -1;
+        if (titleA > titleB) return 1;
+      }
+
+      return 0;
+    });
+  }
+}
 
 </script>
 
@@ -256,8 +265,8 @@ function sortProducts() {
                     </button>
                 </div>
 
-                <div v-if="sale_prices" className="grid grid-cols-2 gap-4 p-4" v-auto-animate>
-                    <Item 
+              <div v-if="sale_prices" :class="['grid grid-cols-2 gap-4 p-4', { 'animate-fade': animating }]">
+                    <Item
                         v-for="game, index in games"
                         :key="index"
                         :product="game"
@@ -275,3 +284,25 @@ function sortProducts() {
         </transition>
     </main>
 </template>
+
+<style scoped>
+.grid-cols-2 > * {
+  min-height: 250px;
+}
+
+.animate-fade {
+  animation: fadeOutIn 0.5s ease-in-out;
+}
+
+@keyframes fadeOutIn {
+  0% {
+    opacity: 1;
+  }
+  50% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+</style>
