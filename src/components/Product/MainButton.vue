b707c7727d7000be87498f6ade9261771681fd7e
EugeneMazurok <audiofaun896373@gmail.com>
2024-09-20 17:16:39 +0300

    cybersport56

--- src/components/Product/MainButton.vue
+++ src/components/Product/MainButton.vue
@@ -17,11 +17,11 @@ const emit = defineEmits(['submit'])
 <template>
   <span :class="isFixed ? ['flex h-20'] : ''" />
   <div :class="['justify-center flex', isFixed ? 'fixed bottom-0 left-0 right-0 bg-secondary_bg_color' : 'static']">
-    <div class="min-512:w-[512px] w-full min-512:rounded-t-xl flex justify-center items-center">
+    <div :class="['min-512:w-[512px] w-full min-512:rounded-t-xl flex justify-center items-center', isFixed ? 'p-4' : '']">
       <button
           :disable="buttonLoader"
           :style="{ backgroundColor: color }"
-          :class="[isFixed ? 'p-4' : '', 'w-full px-2 h-12 flex justify-center items-center rounded-xl shadow-sm font-medium active:opacity-75']"
+          class="w-full px-2 h-12 flex justify-center items-center rounded-xl shadow-sm font-medium active:opacity-75"
           @click="emit('submit')"
       >
         <Icon v-if="buttonLoader" icon="line-md:loading-loop" class="text-lg" />
